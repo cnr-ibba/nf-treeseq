9dedf2c90166565cff4234db794d429fb3fd0666
Paolo Cozzi <bunop@libero.it>
2024-04-08 21:14:43 +0200

    :sparkles: add scripts in bin folder

---
+++ bin/tskitetude/__init__.py
@@ -0,0 +1,18 @@
+"""
+TSKITetude
+
+An helper library to deal with tskit objects
+"""
+
+import pathlib
+
+__version__ = "0.3.0"
+__author__ = "Paolo Cozzi"
+
+
+def get_project_dir() -> pathlib.PosixPath:
+    mypath = pathlib.Path(__file__)
+    return mypath.parents[1]
+
+def get_data_dir() -> pathlib.PosixPath:
+    return get_project_dir() / "data"
